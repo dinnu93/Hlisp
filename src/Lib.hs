17f1187e63105f037e510444a3e49ac96ad800a2
dinnu93 <dinesh.padisetti@gmail.com>
2016-06-12 11:25:38 +0530

    Added the expression datatype

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+import qualified Data.Char as C
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- Simple Interpreter for calculator like expressions :)
+
+data LExpr = Lit Integer 
+           | Add LExpr LExpr 
+           | Mul LExpr LExpr 
+           | Sub LExpr LExpr 
+           | Div LExpr LExpr 
+           deriving (Show,Eq)
+
+
+
+
