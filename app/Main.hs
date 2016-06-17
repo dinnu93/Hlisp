9acbbfb8f7735633687472df4cc8180312a7a086
dinnu93 <dinesh.padisetti@gmail.com>
2016-06-17 16:28:56 +0530

    Added some parser generators

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
 import Lib
+import System.Environment
+import System.IO
+import Control.Monad 
 
 main :: IO ()
-main = putStrLn "Nothing!"
+main = forever $ do
+  code <- prompt ">> "
+  putStrLn $ code
+
+prompt :: String -> IO String
+prompt text = do
+  putStr text
+  hFlush stdout
+  getLine
+
+ 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,62 @@
 module Lib where
-import qualified Data.Char as C
 
--- Simple Interpreter for calculator like expressions :)
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
 
-data LExpr = Lit Integer 
-           | Add LExpr LExpr 
-           | Mul LExpr LExpr 
-           | Sub LExpr LExpr 
-           | Div LExpr LExpr 
-           deriving (Show,Eq)
+-- Lisp Abstract Syntax Tree
 
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+             deriving (Show)
 
+-- Parser Combinators
 
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+spaces :: Parser ()
+spaces = skipMany1 space 
+
+-- Parser Combinators returning Lisp Values
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many ((noneOf "\\\"") <|> (char '\\' >> oneOf "\""))
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = do
+  n <- many1 digit
+  return $ Number . read $ n
+
+-- Final Lisp Expression Parser
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+  <|> parseString
+  <|> parseNumber
+
+-- read the expression and give a string 
+
+readExpr :: String -> LispVal
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> error . show $ err
+  Right val -> val
+  
